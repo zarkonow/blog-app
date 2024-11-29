ec5d6d0cb157a655bb992e3b729c28180ee9a857
zarkonow <stojanov1987@gmail.com>
2024-11-29 18:50:58 +0100

    *server added nodmon

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+
+const app =express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/api',(req,resizeBy) => {
+    resizeBy.send('Hello World')
+})
+
+
+app.listen(5000,() => console.log('App is running at 5000...'))
