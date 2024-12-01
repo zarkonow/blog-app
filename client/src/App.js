7218f9fe119ece096542b196200c1dbde1614592
zarkonow <stojanov1987@gmail.com>
2024-12-01 23:14:16 +0100

    *added Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-
-import './App.css';
-import Header from './components/header';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header";
+import Home from "./pages/home";
+import AddNewBlog from "./pages/add-blog";
 
 function App() {
   return (
     <div>
-      <Header/>
-
-
+      <Header />
+      <Routes>
+        <Route exeact path="/" element={<Home />}></Route>
+        <Route exeact path="/add-blog" element={<AddNewBlog />}></Route>
+      </Routes>
     </div>
   );
 }
